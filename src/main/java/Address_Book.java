032fde4360662256d0ddfb4807006a3506bd1a93
kaushiki <kaushiki.ak88@gmail.com>
2021-02-26 14:05:47 +0530

    ADDED: welcome message

---
+++ src/main/java/Address_Book.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Address_Book {
+    //WELCOME MESSAGE
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
+
